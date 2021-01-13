11567f93ddec309d41a4ea49de3e3a1d503830c5
ZZZZ-afk <73995639+ZZZZ-afk@users.noreply.github.com>
2021-01-13 18:02:12 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -28,7 +28,7 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.8, isStatic:false});
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:1, isStatic:false});
 	World.add(world, packageBody);
 
 	//Create a Ground
@@ -40,7 +40,7 @@ function setup() {
 
 function draw() {
   rectMode(CENTER);
-  background(rgb(mouseX,mouseY,0));
+  background(0);
   keyPressed();
   packageBody.y=helicopterSprite.position.y
   drawSprites();
@@ -49,7 +49,6 @@ function draw() {
 function keyPressed() {
  if (keyCode === DOWN_ARROW) {
 	// Look at the hints in the document and understand how to make the package body fall only on press of the Down arrow key.
-	packageSprite.x= packageBody.position.x 
 	packageSprite.y= packageBody.position.y
 }
 }
